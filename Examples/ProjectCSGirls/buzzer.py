0b596c44def752a8b2433ae9dd488d7eac429bda
tproffen <thomasproffen@gmail.com>
2022-01-31 19:19:18 -0500

    Teminate buzzer on exit

--- Examples/ProjectCSGirls/buzzer.py
+++ Examples/ProjectCSGirls/buzzer.py
@@ -4,12 +4,16 @@ from adafruit_seesaw import seesaw, pwmout
 import time
 
 myI2C = busio.I2C(SCL, SDA)
+BUZZER_PIN = 5
 
 ss = seesaw.Seesaw(myI2C)
-pwm = pwmout.PWMOut(ss,5)
+pwm = pwmout.PWMOut(ss,BUZZER_PIN)
 
 while True:
-  pwm.duty_cycle=0x80
-  time.sleep(1)
-  pwm.duty_cycle=0x00
-  time.sleep(1)
+  try:
+    pwm.duty_cycle=0x80
+    time.sleep(1)
+    pwm.duty_cycle=0x00
+    time.sleep(1)
+  finally:
+    pwm.duty_cycle=0x00
