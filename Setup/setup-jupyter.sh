802c3cec6bf2445fedc64f772566c2f29a0ef947
Thomas Proffen <thomasproffen@gmail.com>
2024-02-01 16:50:01 -0500

    Update setup-jupyter.sh

--- Setup/setup-jupyter.sh
+++ Setup/setup-jupyter.sh
@@ -9,23 +9,12 @@ sudo pip3 install jupyter
 sudo pip3 install jupyterlab
 sudo pip3 install jupyterthemes
 
-mkdir -p /home/pi/.jupyter
-cd /home/pi/.jupyter
-
-cat > jupyter_notebook_config.py <<EOF
-# Configuration file for jupyter-notebook.
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 5555
-c.Notebook.open_browser = False
-c.NotebookApp.quit_button = False
-EOF
-
 cat > jupyter.service << EOF
 [Unit]
 After=network.service
 
 [Service]
-ExecStart=/usr/local/bin/jupyter lab --config=/home/pi/.jupyter/jupyter_notebook_config.py
+ExecStart=/usr/local/bin/jupyter lab --ip 0.0.0.0 --port 5555 --no-browser --allow-root
 User=pi
 
 [Install]
